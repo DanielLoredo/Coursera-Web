6689b7c4e3f66372e8e99e9f4e53d81a93ac7baf
DanielLoredo <danjorloredo@gmail.com>
2020-12-15 15:24:47 -0600

    images were added

--- REACT/jsx/src/CommentDetail.js
+++ REACT/jsx/src/CommentDetail.js
@@ -3,13 +3,13 @@ import "./css/bootstrap-grid.min.css";
 import "./css/style.css";
 
 
-const CommentDetail = () => {
+const CommentDetail = props => {
     return (
                 <div class = "col-12 col-sm-12 col-md-6 col-lg-6 normalComment">
                     <div class = "position-relative d-flex flex-row">
-                        <div><img src = "img/corgi.jpg" /></div>
+                        <div><img src = {props.foto} /></div>
                         <div class = "headerComment">
-                            <div class = "nameComment">Daniel Loredo<span class = "dateComment">Diciembre 15, 2020</span></div>
+                            <div class = "nameComment">{props.author}<span class = "dateComment">{props.dateAgo}</span></div>
                             <div class = "descriptionComment">Hola yo soy Daniel muchisimo gusto ojala nos podamos llevar a toda madre</div>
                         </div>
                     </div>
--- REACT/jsx/src/index.js
+++ REACT/jsx/src/index.js
@@ -10,20 +10,20 @@ const App = () => {
     <div>
         <div class = "container">
             <div class = "row">
-                <CommentDetail />
-                <CommentDetail />
+                <CommentDetail author="Daniel Loredo" dateAgo = "Hace unos segundos" foto = "img/daniel.jpg" />
+                <CommentDetail author="Portilla" dateAgo = "Hace 2 horas" foto = "img/portilla.jpg"/>
             </div>
             <div class = "row">
-                <CommentDetail />
-                <CommentDetail />
+                <CommentDetail author="El león Valencia" dateAgo = "Hoy a las 5:00pm" foto = "img/valencia.jpg"/>
+                <CommentDetail author="Jorge Loredo" dateAgo = "Ayer" foto = "img/jorge.jpg"/>
             </div>
             <div class = "row">
-                <CommentDetail />
-                <CommentDetail />
+                <CommentDetail author="Narhariiiiii" dateAgo = "Ultima conexión en 2010 :(" foto = "img/narhari.jpg" />
+                <CommentDetail author="MOIK" dateAgo = "Ayer a las 2:00pm" foto = "img/moik.jpg"/>
             </div>
             <div class = "row">
-                <CommentDetail />
-                <CommentDetail />
+                <CommentDetail author="Cesitar" dateAgo = "Inifinito" foto = "img/cesar.jpg"/>
+                <CommentDetail author="Urcadiz" dateAgo = "Ayer" foto = "img/urcadiz.jpg"/>
             </div>
         </div>
             
