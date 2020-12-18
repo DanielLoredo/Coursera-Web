9814b737c7aeed3f03361893b60825e43bfee26d
DanielLoredo <danjorloredo@gmail.com>
2020-12-18 14:23:44 -0600

    add style

--- REACT/pics/src/components/Images.js
+++ REACT/pics/src/components/Images.js
@@ -2,10 +2,27 @@ import React from "react";
 import "./imageStyle.css";
 
 class Images extends React.Component {
+    constructor (props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+        this.state = {spans: 0};
+    }
+
+    componentDidMount () {
+        this.imageRef.current.addEventListener("load", this.setSpans)
+    }
+
+    setSpans = () => {
+        const spans = Math.ceil(this.imageRef.current.clientHeight / 10);
+        this.setState({ spans });
+    }
+
     render() {
         return (
-            <div>
+            <div style = {{gridRowEnd: `span ${this.state.spans}`}}>
                 <img
+                    ref = {this.imageRef}
                     alt = {this.props.onImages.description}
                     src = {this.props.onImages.urls.regular}
                 />
