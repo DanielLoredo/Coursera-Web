2462a9b72f0934a70312be8ddd46cb066562c7e8
DanielLoredo <danjorloredo@gmail.com>
2020-12-27 14:06:35 -0600

    using Redux

---
+++ REACT/songs/src/reducers/index.js
@@ -0,0 +1,16 @@
+const songReducer = () => {
+    return [
+        {title: "Macarena", duration: "4:05"},
+        {title: "Ahora dice", duration: "3:49"},
+        {title: "La vuelta al mundo", duration: "2:17"},
+        {title: "Atrevete", duration: "1:58"}
+    ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === "SONG_SELECTED"){
+        return action.payload;
+    }
+
+    return selectedSong;
+};
