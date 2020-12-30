6bc2c03634fca04bf23021264dda4208b73ae80c
DanielLoredo <danjorloredo@gmail.com>
2020-12-30 14:23:38 -0600

    Using Redux-Thunk

--- REACT/blog/src/components/PostsList.js
+++ REACT/blog/src/components/PostsList.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchPosts } from "../actions/index";
+import { fetchPostsAndUsers } from "../actions/index";
+import UserHeader from "./UserHeader";
+
 import "../css/bootstrap.min.css";
 import "../css/style.css";
 
+
 class PostsList extends React.Component {
     componentDidMount () {
-        this.props.fetchPosts();
+        this.props.fetchPostsAndUsers();
     }
     
     renderList () {
         return (this.props.posts.map((post) => {
+
             return(
                 <div key = {post.id} className="card">
                     <div className="card-body"> 
@@ -20,6 +24,7 @@ class PostsList extends React.Component {
                                     <div>
                                         <div style = {{fontWeight: "bold", fontSize: "30px"}}>{post.title}1</div>
                                         <div>{post.body}2</div>
+                                        <UserHeader userId = {post.userId} />
                                     </div>
                             </div>
                         </p>
@@ -42,4 +47,4 @@ const mapToStateProps = (state) => {
     return ({posts: state.posts});
 }
 
-export default connect(mapToStateProps, { fetchPosts: fetchPosts })(PostsList);
+export default connect(mapToStateProps, { fetchPostsAndUsers: fetchPostsAndUsers })(PostsList);
---
+++ REACT/blog/src/components/UserHeader.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { connect } from "react-redux";
+
+class UserHeader extends React.Component {
+    render () {
+        const { user } = this.props;
+
+        if(!user){
+            return null;
+        }
+
+        return <div style = {{fontWeight: "bold"}}>{user.name}</div>
+    }
+}
+
+const mapToStateProps = (state, ownProps) => {
+    return ({ user: state.users.find(user => user.id === ownProps.userId)});
+}
+
+export default connect(mapToStateProps)(UserHeader);
