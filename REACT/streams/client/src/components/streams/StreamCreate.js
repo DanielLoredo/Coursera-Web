b2341e7952a0948f0b1872fc22c0225b03b25e82
DanielLoredo <danjorloredo@gmail.com>
2021-01-05 14:44:10 -0600

    REST + api

--- REACT/streams/client/src/components/streams/StreamCreate.js
+++ REACT/streams/client/src/components/streams/StreamCreate.js
@@ -1,20 +1,33 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
 import "../../css/bootstrap.min.css";
+import { connect } from "react-redux";
+import { createStreams } from "../../actions/index";
 
 class StreamCreate extends React.Component {
 
-    renderInput (formProps) {
+    renderedError (meta) {
+        if(meta.touched && meta.error) {
+            return (
+                <div>
+                    {meta.error}
+                </div>
+            );
+        }
+    }
+
+    renderInput = (formProps) => {
         return (
             <div className = "form-group">
                 <label style = {{fontWeight: "bold"}}>{formProps.label}</label>
-                <input className="form-control" {...formProps.input} />
+                <input autoComplete="off" className="form-control" {...formProps.input} />
+                <div>{this.renderedError(formProps.meta)}</div>
             </div>
         );
     }
 
-    onSubmit (formValues) {
-        console.log(formValues);
+    onSubmit = (formValues) => {
+        this.props.createStreams(formValues);
     }
 
     render() {
@@ -27,6 +40,21 @@ class StreamCreate extends React.Component {
         );
     };
 }
-export default reduxForm({
-    form: "streamCreate"
+
+const validate = (formValues) => {
+    const errors = {};
+    if(!formValues.title) {
+        errors.title = "You must enter a title";
+    }
+    if(!formValues.description) {
+        errors.description = "You must enter a description";
+    }
+    return errors;
+};
+
+const formWrapped = reduxForm({
+    form: "streamCreate",
+    validate: validate
 })(StreamCreate);
+
+export default connect(null, { createStreams })(formWrapped);
--- REACT/streams/client/src/index.js
+++ REACT/streams/client/src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
+import ReduxThunk from "redux-thunk";
 
 import App from "./components/App";
 import Reducers from "./reducers";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(Reducers, composeEnhancers(applyMiddleware()));
+const store = createStore(Reducers, composeEnhancers(applyMiddleware(ReduxThunk)));
 
 
 ReactDOM.render(
