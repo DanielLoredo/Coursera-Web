00950198cac24a88381fb4e933c607bb95ced260
DanielLoredo <danjorloredo@gmail.com>
2021-01-06 11:33:44 -0600

    streamReducer

--- REACT/streams/client/src/components/streams/StreamList.js
+++ REACT/streams/client/src/components/streams/StreamList.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { fetchStreams } from "../../actions";
+import { connect } from "react-redux";
+import "../../css/bootstrap.min.css";
 
-const StreamList = () => {
-    return <div>StreamList</div>;
+class StreamList extends React.Component {
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
+
+    renderList () {
+        return this.props.streams.map((stream) => {
+            return (
+                <li key = {stream.description} className = "list-group-item">
+                    <div style = {{fontWeight: "bold", fontSize: "25px"}}>{stream.title}</div>
+                    
+                    {stream.description}
+                </li>
+            );
+        });
+    }
+
+    render () {
+        return (
+            <ul className = "list-group" style = {{marginTop: "20px"}}>
+                {this.renderList()}
+            </ul>
+        );
+    }
 }
 
-export default StreamList;
+const mapStateToProps = (state) => {
+    console.log(state);
+    return { streams: Object.values(state.streams) };
+}
+
+export default connect(mapStateToProps, {fetchStreams})(StreamList);
