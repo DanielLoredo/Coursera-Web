2876f3fdaf9e55a6a65d2ae3aafd369eca277383
DanielLoredo <danjorloredo@gmail.com>
2021-01-09 11:10:26 -0600

    Animations

--- CSS-advanced/dogs/src/components/Header.js
+++ CSS-advanced/dogs/src/components/Header.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "../css/headerStyle.css";
+import imgDaniel from '../img/daniel-icon.jpg';
 
 class Header extends React.Component {
     render () {
         return (
-            <header className = "header" />
+            <header className = "header" >
+                <div className = "logo-box">
+                    <img src = {imgDaniel} className = "logo" alt = "logo" ></img>
+                </div>
+                <div className = "title-box">
+                    <h1 className = "text-content">
+                        <span className = "title-main-primary">Servicio TI</span>
+                        <span className = "title-main-sub">Soporte Técnico del bueno</span>
+                    </h1>
+
+                    <a href = "#" className = "btn btn-white btn-animated">Llama Ya</a>
+                </div>
+            </header>
         );
     }
 }
