c7b2bd72aae845903ef0f9c0fd7cce80e7985b06
DanielLoredo <danjorloredo@gmail.com>
2021-01-20 19:36:49 -0600

    auth

--- Node/4-natours/starter/app.js
+++ Node/4-natours/starter/app.js
@@ -1,9 +1,9 @@
-// MIDLEWARE AND EXPRESS CONECTION FILE
 const express = require('express');
 const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
+const AppError = require('./utils/appError');
 
 const app = express(); //use express
 
@@ -16,16 +16,28 @@ app.use(express.json()); //middleWare to allow post requests
 // app.use(express.static(`${__dirname}/public`)); ADD AN STATIC FILE
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// ERROR HANDLING
+app.all('*', (req, res, next) => {
+  next(new AppError(`Cannot found ${req.originalUrl} on the server`), 404);
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
